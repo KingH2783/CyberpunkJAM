139e0fc7a727942a7f4f50349eff18fa8e525965
King_H <hp.king2783@gmail.com>
2024-05-27 18:02:29 +0100

    Merge from Main + Player and AI animations
    Issues that remain:
    - Player running and shooting still shuffles
    - AI does not reload
    - AI only shoots one bullet
    - There is no knockback on the player

--- Cyberpunk/Assets/Scripts/Characters/CharacterSoundFXManager.cs
+++ Cyberpunk/Assets/Scripts/Characters/CharacterSoundFXManager.cs
@@ -4,6 +4,17 @@ namespace HL
 {
     public class CharacterSoundFXManager : MonoBehaviour
     {
+        [SerializeField] private AK.Wwise.Event Footsteps;
+        [SerializeField] private AK.Wwise.Event PlasmaGunShoot;
 
+        public void PlayFootsteps()
+        {
+            Footsteps.Post(gameObject);
+        }
+
+        public void PlayPlasmaGunShoot()
+        {
+            PlasmaGunShoot.Post(gameObject);
+        }
     }
 }
--- Cyberpunk/Assets/Scripts/Characters/Player/PlayerSoundFXManager.cs
+++ Cyberpunk/Assets/Scripts/Characters/Player/PlayerSoundFXManager.cs
@@ -4,17 +4,6 @@ namespace HL
 {
     public class PlayerSoundFXManager : CharacterSoundFXManager
     {
-        [SerializeField] private AK.Wwise.Event Footsteps;
-        [SerializeField] private AK.Wwise.Event PlasmaGunShoot;
-
-        public void PlayFootsteps()
-        {
-            Footsteps.Post(gameObject);
-        }
-
-        public void PlayPlasmaGunShoot()
-        {
-            PlasmaGunShoot.Post(gameObject);
-        }
+        
     }
 }
