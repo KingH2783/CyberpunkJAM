59f94ac16ab590ca51c578985d85633424b1fff5
pedrohcj <pedrohcj89@gmail.com>
2024-06-03 15:19:37 +0100

    push

--- Cyberpunk/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Cyberpunk/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -21,6 +21,9 @@ namespace AK
         static const AkUniqueID FOOTSTEPS = 2385628198U;
         static const AkUniqueID PLASMA_GUN_SHOOT = 2851466780U;
         static const AkUniqueID PLAYER_DASH = 2394582229U;
+        static const AkUniqueID PLAYER_DEATH = 3083087645U;
+        static const AkUniqueID PLAYER_LAND = 3629196698U;
+        static const AkUniqueID PLAYER_RAISED_GUN_RUNNING = 640208366U;
         static const AkUniqueID PLAYER_RANGED_ATTACK_ATTACK1_STATIONARY = 165391104U;
         static const AkUniqueID PLAYER_RELOAD = 1650679582U;
         static const AkUniqueID STOP_TEST = 2051061508U;
