c277d1916d323e6b7dc5ff54f9b042e6f5bfff07
King_H <hp.king2783@gmail.com>
2024-06-19 19:12:46 +0100

    Advanced NPC Dialogue Functionality

--- Cyberpunk/Assets/Scripts/UI/InteractableNPC.cs
+++ Cyberpunk/Assets/Scripts/UI/InteractableNPC.cs
@@ -4,32 +4,63 @@ namespace HL
 {
     public class InteractableNPC : Interactable
     {
-        [SerializeField] private Sprite NPCPortrait;
-        [SerializeField] private string NPCName;
-        [SerializeField][TextArea] private string[] NPCDialogue;
+        [SerializeField] private NPCDialogue[] NPCDialogues;
 
         private int interactionIndex = 0;
+        private int textIndex = 0;
 
         public override void Interact(PlayerManager player)
         {
             base.Interact(player);
 
-            if (interactionIndex == 0)
+            while (interactionIndex < NPCDialogues.Length)
             {
-                PlayerUIManager.Instance.npcDialogueUI.EnableNPCDialogue(NPCPortrait, NPCName);
-                PlayerUIManager.Instance.npcDialogueUI.UpdateNPCDialogue(NPCDialogue[interactionIndex]);
-                interactionIndex++;
+                NPCDialogue currentDialogue = NPCDialogues[interactionIndex];
+
+                if (textIndex == 0)
+                {
+                    PlayerUIManager.Instance.npcDialogueUI.EnableNPCDialogue(currentDialogue.NPCPortrait, currentDialogue.NPCName);
+                    PlayerUIManager.Instance.npcDialogueUI.UpdateNPCDialogue(currentDialogue.NPCDialogueText[textIndex]);
+                    textIndex++;
+                    break;
+                }
+                else if (textIndex < currentDialogue.NPCDialogueText.Length)
+                {
+                    PlayerUIManager.Instance.npcDialogueUI.UpdateNPCDialogue(currentDialogue.NPCDialogueText[textIndex]);
+                    textIndex++;
+                    break;
+                }
+                else
+                {
+                    // Move to the next dialogue struct
+                    interactionIndex++;
+                    textIndex = 0;
+
+                    if (interactionIndex < NPCDialogues.Length)
+                    {
+                        currentDialogue = NPCDialogues[interactionIndex];
+                        PlayerUIManager.Instance.npcDialogueUI.EnableNPCDialogue(currentDialogue.NPCPortrait, currentDialogue.NPCName);
+                        PlayerUIManager.Instance.npcDialogueUI.UpdateNPCDialogue(currentDialogue.NPCDialogueText[textIndex]);
+                        textIndex++;
+                        break;
+                    }
+                }
             }
-            else if (interactionIndex < NPCDialogue.Length)
-            {
-                PlayerUIManager.Instance.npcDialogueUI.UpdateNPCDialogue(NPCDialogue[interactionIndex]);
-                interactionIndex++;
-            }
-            else
+
+            // Disable the UI if we've reached the end of all dialogues
+            if (interactionIndex >= NPCDialogues.Length)
             {
                 PlayerUIManager.Instance.npcDialogueUI.DisableNPCDialogue();
                 interactionIndex = 0;
             }
+        }
+
+        [System.Serializable]
+        private struct NPCDialogue
+        {
+            public Sprite NPCPortrait;
+            public string NPCName;
+            [TextArea] public string[] NPCDialogueText;
         }
     }
 }
